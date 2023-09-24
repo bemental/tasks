dab6330315ea61c8a6968732ac1a026f1f353168
bemental <bemental@users.noreply.github.com>
2023-09-24 06:24:32 -0400

    toShortForm function updated
    toShortForm function passes tests

--- src/objects.ts
+++ src/objects.ts
@@ -61,7 +61,8 @@ export function isValid(question: Question, answer: string): boolean {
  * name "My First Question" would become "9: My First Q".
  */
 export function toShortForm(question: Question): string {
-    return "";
+    const shortenedName = question.name.substr(0, 10);
+    return `${question.id}: ${shortenedName}`;
 }
 
 /**
