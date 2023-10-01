05fe72630ca90f0ca944d68a01e139556d3b3fa6
bemental <bemental@users.noreply.github.com>
2023-10-01 07:27:23 -0400

    sumPublishedPoints function updated
    sumPublishedPoints passes tests

--- src/nested.ts
+++ src/nested.ts
@@ -67,7 +67,9 @@ export function sumPoints(questions: Question[]): number {
  * Consumes an array of questions and returns the sum total of the PUBLISHED questions.
  */
 export function sumPublishedPoints(questions: Question[]): number {
-    return 0;
+    return questions
+        .filter(question => question.published)
+        .reduce((total, question) => total + question.points, 0);
 }
 
 /***
