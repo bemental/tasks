bdc6016429bd4dbaa0e7f6590c410ab0e13a4227
bemental <bemental@users.noreply.github.com>
2023-10-01 07:29:46 -0400

    makeAnswers function updated
    makeAnswers passes tests

--- src/nested.ts
+++ src/nested.ts
@@ -113,7 +113,12 @@ export function toCSV(questions: Question[]): string {
  * making the `text` an empty string, and using false for both `submitted` and `correct`.
  */
 export function makeAnswers(questions: Question[]): Answer[] {
-    return [];
+    return questions.map(question => ({
+        questionId: question.id,
+        text: '',
+        submitted: false,
+        correct: false
+    }));
 }
 
 /***
